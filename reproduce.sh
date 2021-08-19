eca8d784bb4dd14e8ff38e765d41799be4a2fb9d
Max Fan <root@max.fan>
2021-08-19 15:38:48 -0500

    Add full reproduction script

--- reproduce.sh
+++ reproduce.sh
@@ -8,3 +8,17 @@ python migrate.py NM ../NM-shapefiles/new_mexico_precincts.zip --vtd-loc ../nm-p
 python migrate.py MA ~/git/VRA-data-products/Massachusetts/shapes/MA_pcts.shp
 python migrate.py MD ../MD-shapefiles/MD_precincts_abs.zip
 python migrate.py LA ~/git/LA-shapefiles/LA_1519.zip
+python migrate.py TX source/TX_vtds.zip
+python migrate.py CT ../CT-shapefiles/CT_precincts.zip
+python migrate.py DE ../DE-shapefiles/DE_precincts.zip
+python migrate.py GA ../GA-shapefiles/GA_precincts.zip
+python migrate.py NC ../NC-shapefiles/NC_VTD.zip
+python migrate.py MN ../MN-shapefiles/MN12_18.zip
+python migrate.py NE ../NE-shapefiles/NE.zip
+python migrate.py NH ../NH-shapefiles/NH.zip
+python migrate.py OK ../OK-shapefiles/OK_precincts.zip
+python migrate.py RI ../RI-shapefiles/RI_precincts.zip
+python migrate.py WI ../WI-shapefiles/WI_2020_wards.zip
+
+
+
