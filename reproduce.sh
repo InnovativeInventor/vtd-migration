99750595c0a6f3a9489b7fb70e846eab4728d51c
Max Fan <root@max.fan>
2021-08-19 14:32:56 -0500

    Add reproduce script

---
+++ reproduce.sh
@@ -0,0 +1,10 @@
+python migrate.py MI ~/git/MI-shapefiles/MI.zip
+python migrate.py VA ~/git/VA-shapefiles/VA_precincts.zip
+python migrate.py AK ~/git/AK-shapefiles/AK_precincts.zip --repair
+python migrate.py IN ~/git/IN-shapefiles/Indiana.zip
+python migrate.py UT ~/git/UT-shapefiles/UT_precincts.zip
+python migrate.py AZ ~/git/AZ-shapefiles/az_precincts.zip
+python migrate.py NM ../NM-shapefiles/new_mexico_precincts.zip --vtd-loc ../nm-precincts/nm_precincts.shp --output-loc products/nm_precincts_with_elections.shp
+python migrate.py MA ~/git/VRA-data-products/Massachusetts/shapes/MA_pcts.shp
+python migrate.py MD ../MD-shapefiles/MD_precincts_abs.zip
+python migrate.py LA ~/git/LA-shapefiles/LA_1519.zip
